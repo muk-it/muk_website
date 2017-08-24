c311d5dcf348325d6aafcc5700f6d083d6757f66
Mathias Markl <mathias.markl@mukit.at>
2017-08-24 15:21:18 +0200

    init

---
+++ muk_theme_nav_transparent/README.md
@@ -0,0 +1,3 @@
+# Transparent Navigation Menu
+
+Adds a option to the website customize menu to trigger the transparent navigation menu.
---
+++ muk_theme_nav_transparent/static/src/js/navbar.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+ 
+    // check if logged in
+	if($('#oe_main_menu_navbar').length >= 1) {
+		$('header').addClass("muk_not_editable");
+	}
+	
+	$(window).scroll(function() {
+	    if ($(".muk_navbar_transparent").offset().top > 50) {
+	        $('.muk_navbar_transparent').addClass('affix');
+	        $(".muk_navbar_transparent").addClass("top-nav-collapse");
+	    } else {
+	        $('.muk_navbar_transparent').removeClass('affix');
+	        $(".muk_navbar_transparent").removeClass("top-nav-collapse");
+	    }
+	});
+
+});
