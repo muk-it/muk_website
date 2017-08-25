32e9165e0f25b2c07e4b88e1bb1a46883a438712
Mathias Markl <mathias.markl@mukit.at>
2017-08-25 12:51:52 +0200

    update

--- muk_theme_nav_transparent/static/src/js/navbar.js
+++ muk_theme_nav_transparent/static/src/js/navbar.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
  
     // check if logged in
-	if($('#oe_main_menu_navbar').length >= 1) {
-		$('header').addClass("muk_not_editable");
+	if($("#oe_main_menu_navbar").length >= 1) {
+		$("header").addClass("muk_not_editable");
 	}
 	
 	$(window).scroll(function() {
 	    if ($(".muk_navbar_transparent").offset().top > 50) {
-	        $('.muk_navbar_transparent').addClass('affix');
+	        $(".muk_navbar_transparent").addClass("affix");
 	        $(".muk_navbar_transparent").addClass("top-nav-collapse");
 	    } else {
-	        $('.muk_navbar_transparent').removeClass('affix');
+	        $(".muk_navbar_transparent").removeClass("affix");
 	        $(".muk_navbar_transparent").removeClass("top-nav-collapse");
 	    }
 	});
