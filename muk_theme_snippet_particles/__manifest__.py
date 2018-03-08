67551dc7f351f043653cbcd0e36ce7cf0ed0defa
Mathias Markl <mathias.markl@mukit.at>
2018-03-08 13:44:02 +0100

    Update __manifest__.py

--- muk_theme_snippet_particles/__manifest__.py
+++ muk_theme_snippet_particles/__manifest__.py
@@ -28,7 +28,7 @@
         of customization options. 
     """,
     "version": "11.0.1.0.0",
-    "category": "Website",
+    "category": "Hidden",
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
     "author": "MuK IT",
