be70edecce40b5be5dc7c454a8b4f6ab39e0288c
Mathias Markl <mathias.markl@mukit.at>
2018-03-13 00:21:00 +0100

    1.0.0

---
+++ muk_website_snippet_grid/README.md
@@ -0,0 +1,50 @@
+MuK Grid Snippets
+===============
+
+Bootstrap includes a responsive, mobile first fluid grid system that appropriately
+scales up to 12 columns as the device or viewport size increases.
+The grid systems can be used to create page layouts through a series of rows and
+columns that house your content.
+
+Installation
+============
+
+To install this module, you need to:
+
+Download the module and add it to your Odoo addons folder. Afterward, log on to
+your Odoo server and go to the Apps menu. Trigger the debug modus and update the
+list by clicking on the "Update Apps List" link. Now install the module by
+clicking on the install button.
+
+Configuration
+============
+
+No additional configuration is needed to use this module.
+
+Usage
+============
+
+Switch to your website and switch to the edit mode. Now four new snippets will
+appear in your sidebar.
+
+Credits
+============
+
+Contributors
+------------
+
+-	Mathias Markl &lt;<mathias.markl@mukit.at>&gt;
+
+Author & Maintainer
+------------
+
+This module is maintained by the [MuK IT GmbH].
+
+MuK IT is an Austrian company specialized in customizing and extending
+Odoo. We develop custom solutions for your individual needs to help you
+focus on your strength and expertise to grow your business.
+
+If you want to get in touch please contact us via mail (<sale@mukit.at>)
+or visit our website (<https://mukit.at>).
+
+  [MuK IT GmbH]: https://www.mukit.at/
---
+++ muk_website_snippet_grid/__manifest__.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+###################################################################################
+# 
+#    Copyright (C) 2017 MuK IT GmbH
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+{
+    "name": "MuK Grid Snippets",
+    "summary": """Bootstrap Grid System""",
+    "description": """ 
+        Bootstrap includes a responsive, mobile first fluid
+        grid system that appropriately scales up to 12
+        columns as the device or viewport size increases.
+        The grid systems can be used to create page layouts
+        through a series of rows and columns that house
+        your content.
+    """,
+    "version": "11.0.1.0.0",
+    "category": "Website",
+    "license": "AGPL-3",
+    "website": "http://www.mukit.at",
+    "author": "MuK IT",
+    "contributors": [
+        "Mathias Markl <mathias.markl@mukit.at>",
+    ],
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "template/assets.xml",
+        "snippets/grid.xml",
+        "snippets/snippets.xml",
+        "snippets/options.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+        "static/src/xml/*.xml",
+    ],
+    "images": [
+        'static/description/banner.png'
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "application": False,
+    "installable": True,
+    
+}
