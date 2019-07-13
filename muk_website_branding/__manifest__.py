d80ab0c584891d0da1f91d398d8328df65e4d77a
MuK IT GmbH <mathias.markl@mukit.at>
2019-07-13 12:32:54 +0000

    publish muk_website_branding - 12.0

--- muk_website_branding/__manifest__.py
+++ muk_website_branding/__manifest__.py
@@ -23,7 +23,7 @@
 {
     "name": "MuK Website Branding",
     "summary": """Branding and Debranding""",
-    "version": "12.0.2.0.0",
+    "version": "12.0.2.0.1",
     "category": "Website",
     "license": "LGPL-3",
     "website": "http://www.mukit.at",
