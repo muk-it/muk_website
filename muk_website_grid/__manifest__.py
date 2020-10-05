72ec4a6d19f99023f1dd84dcbd285e12985f0ca9
Mathias Markl <mathias.markl@mukit.at>
2020-10-05 23:43:22 +0200

    push

---
+++ muk_website_grid/__manifest__.py
@@ -0,0 +1,35 @@
+{
+    'name': 'MuK Grid Snippets',
+    'summary': 'Bootstrap Grid System',
+    'version': '14.0.1.0.0',
+    'category': 'Website',
+    'license': 'LGPL-3',
+    'author': 'MuK IT',
+    'website': 'http://www.mukit.at',
+    'contributors': [
+        'Mathias Markl <mathias.markl@mukit.at>',
+    ],
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'template/assets.xml',
+        'template/snippets.xml',
+        'template/options.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'images': [
+        'static/description/banner.png'
+    ],
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'application': False,
+    'installable': True,
+    
+}
